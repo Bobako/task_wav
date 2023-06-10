49d08b44895c698ab77b0d1d5b299fa53511372f
bobako <abqu144@gmail.com>
2023-06-10 17:08:49 +0300

    env vars for docker

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -7,10 +8,20 @@ from flask_sqlalchemy import SQLAlchemy
 config = configparser.ConfigParser()
 config.read("config.ini")
 
+if os.environ.get("USE_ENV"):
+    config = {key:dict(section) for key, section in dict(config).items()}
+    config["DATABASE"]["uri"] = os.environ.get("DATABASE_URI")
+    config["SITE"]["port"] = os.environ.get("SITE_PORT")
+    config["SITE"]["upload_folder"] = os.environ.get("SITE_UPLOAD_FOLDER")
+
+
 app = Flask(__name__)
 app.secret_key = config["SITE"]["secret_key"]
 app.config['SQLALCHEMY_DATABASE_URI'] = config["DATABASE"]["uri"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = config["SITE"]["upload_folder"]
+app.config['SERVER_NAME'] = config["SITE"]["server_name"]
+
 db = SQLAlchemy(app)
 
 
